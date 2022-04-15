48d6de79300e25644dc740b814f52036b476ae06
Sallie McKee <salliemckee@Morrigan.local>
2022-04-15 13:21:59 -0500

    set up first action router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,16 @@
 // Write your "actions" router here!
+const express = require("express");
+const Actions = require("./actions-model");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Actions.get(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
